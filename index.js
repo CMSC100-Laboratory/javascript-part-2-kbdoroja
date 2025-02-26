2a8da65998656fb5f179c9e6571fe3e7304727d7
kbdoroja <kbdoroja@up.edu.ph>
2025-02-26 18:43:58 +0800

    Features: Added fully functioning addAccount(), as well as implemented appendFileSync()

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import validator from 'validator'
 import { v4 as uuidv4 } from 'uuid';
+import { appendFileSync } from 'node:fs';
 
 const generateUID = (fname, lname) => {
     var generated = uuidv4().slice(0,8); 
@@ -12,8 +13,19 @@ const generateUID = (fname, lname) => {
 }
 
 const addAccount = (Array) => {
+    if(Array.length == 4){
+        if(Array[0] != 0 && Array[1] != 0){
+            if(validator.isEmail(Array[2])){
+                if(Array[3] >= 18){
+                    appendFileSync("user.txt", Array[0]+","+Array[1]+","+Array[2]+","+Array[3]+","+generateUID(Array[0], Array[1]), "utf-8")
+                    
+                    return true; 
+                }
+            }
+        }
+    }
 
-
+    return false; 
 }
 
 export default {generateUID, addAccount}
--- test.js
+++ test.js
@@ -2,3 +2,9 @@ import hey from './index.js'
 
 
 console.log(hey.generateUID('Keophora', 'Silvius')); 
+console.log(hey.addAccount(["Keophora", "Silvius", "keosil@gmail.com", 24]));
+console.log(hey.addAccount(["Bruteforce", "", "iloverocks@gmail.com", 23])); //empty string
+console.log(hey.addAccount(["Franzine", "Fire", "notanarsonist@gmail.com", 17])); //below 18
+console.log(hey.addAccount(["Zvezda", "Stars", "rockstarbaby", 29])); //not valid email
+
+
